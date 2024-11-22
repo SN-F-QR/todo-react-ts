e6ee7667e66c67cd964cc6ad08eb8d8025643b36
SN-F-QR <43869889+SN-F-QR@users.noreply.github.com>
2024-11-22 18:33:30 +0900

    change ReactDOM render to fit react 18

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,9 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./components/App";
 
 // renders React Component "Root" into the DOM element with ID "root"
-ReactDOM.render(<App />, document.getElementById("root"));
+const root = createRoot(document.getElementById("root")!);
+root.render(<App />);
 
 // allows for live updating
 if (module.hot !== undefined) {

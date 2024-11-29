b087292d194799dfcc9bf40c8d2307de70cdaa6f
SN-F-QR <43869889+SN-F-QR@users.noreply.github.com>
2024-11-29 19:13:16 +0900

    feat: GET and POST api for Todo (tasks)

--- server/api.ts
+++ server/api.ts
@@ -1,6 +1,10 @@
 import express from "express";
 import auth from "./auth";
 import socketManager from "./server-socket";
+
+import Task from "./models/Task";
+import { Task as TaskInterface } from "../shared/types";
+
 const router = express.Router();
 
 router.post("/login", auth.login);
@@ -21,9 +25,28 @@ router.post("/initsocket", (req, res) => {
   res.send({});
 });
 
-// |------------------------------|
-// | write your API methods below!|
-// |------------------------------|
+router.get("/todos", auth.ensureLoggedIn, (req, res) => {
+  const getTodos = async (userID: string) => {
+    const todos: TaskInterface[] | null = await Task.find({ creator_id: userID });
+    return res.send(todos); // return TaskInterface[] instead of mere data
+  };
+  getTodos(req.user!._id);
+});
+
+router.post("/todo", auth.ensureLoggedIn, (req, res) => {
+  const newTask = new Task({
+    creator_id: req.user!._id,
+    title: req.body.title,
+    finished: req.body.finished,
+    date: Date.now(),
+  });
+
+  const addTodo = async () => {
+    const task: TaskInterface | null = await newTask.save();
+    return res.send(task);
+  };
+  addTodo();
+});
 
 // anything else falls to this "not found" case
 router.all("*", (req, res) => {

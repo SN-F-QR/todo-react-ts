590d7a2201c33617b685ff37a9a5e7c7665f8050
SN-F-QR <43869889+SN-F-QR@users.noreply.github.com>
2024-11-29 19:15:20 +0900

    fix: Add finished state to Task interface

--- server/models/Task.ts
+++ server/models/Task.ts
@@ -3,12 +3,14 @@ import { Schema, model, Document } from "mongoose";
 const TaskSchema = new Schema({
   creator_id: String,
   title: String,
+  finished: Boolean,
   date: Date,
 });
 
 export interface Task extends Document {
   creator_id: string;
   title: string;
+  finished: boolean;
   date: Date;
 }
 
